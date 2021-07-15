c9d78d20044485a3dd514417b3805af27b98749c
xiongq <flyxiongq@gmail.com>
2021-07-15 23:37:33 +0800

    新增或修改

---
+++ lib/common/widget/button.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/common/utils/utils.dart';
+import 'package:flutter_application_1/common/values/values.dart';
+
+Widget btnFlatButtonWidget({
+  required VoidCallback onPressed,
+  required String title,
+  Color gbColor = AppColors.primaryElement,
+  Color fontColor = AppColors.primaryElementText,
+  double fontSize = 18,
+  String fontName = "Montserrat",
+  FontWeight fontWeight = FontWeight.w400,
+}) {
+  return TextButton(
+    onPressed: () {},
+    child: Text(
+      title,
+      style: TextStyle(
+        color: fontColor,
+        fontFamily: fontName,
+        fontWeight: fontWeight,
+        fontSize: duSetFontSize(fontSize),
+        height: 1,
+      ),
+    ),
+    style: ButtonStyle(
+      backgroundColor: MaterialStateProperty.resolveWith((states) => gbColor),
+      shape: MaterialStateProperty.all(
+        RoundedRectangleBorder(borderRadius: Radii.k6pxRadius),
+      ),
+    ),
+  );
+}
+
+Widget btnImageButtonWidget({
+  required VoidCallback onPressed,
+  required String iconFileName,
+}) {
+  return TextButton(
+    onPressed: () {},
+    child: Image.asset(
+      "assets/images/icons-$iconFileName.png",
+    ),
+    style: ButtonStyle(
+      // backgroundColor: MaterialStateProperty.resolveWith((states) => gbColor),
+      shape: MaterialStateProperty.all(
+        RoundedRectangleBorder(borderRadius: Radii.k6pxRadius),
+      ),
+      side: MaterialStateProperty.all(BorderSide(
+        color: Color.fromARGB(255, 230, 230, 231),
+        width: 1,
+        style: BorderStyle.solid,
+      )),
+    ),
+  );
+}
---
+++ lib/pages/sign_in/sign_in.dart
@@ -0,0 +1,239 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/common/utils/utils.dart';
+import 'package:flutter_application_1/common/values/values.dart';
+import 'package:flutter_application_1/common/widget/button.dart';
+import 'package:flutter_application_1/common/widget/input.dart';
+
+class SignInPage extends StatefulWidget {
+  const SignInPage({Key? key}) : super(key: key);
+
+  @override
+  _SignInPageState createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  final TextEditingController _emailController = TextEditingController();
+  //密码的控制器
+  final TextEditingController _passController = TextEditingController();
+
+  // logo
+  Widget _buildLogo() {
+    return Container(
+      width: duSetWidth(110),
+      margin: EdgeInsets.only(top: duSetHeight(88)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            width: duSetWidth(76),
+            height: duSetWidth(76),
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Positioned(
+                  left: 0,
+                  top: 0,
+                  right: 0,
+                  child: Container(
+                    height: duSetWidth(76),
+                    decoration: BoxDecoration(
+                        color: AppColors.primaryBackground,
+                        boxShadow: [
+                          Shadows.primaryShadow,
+                        ],
+                        borderRadius: BorderRadius.all(
+                            Radius.circular(duSetWidth(76 * 0.5)))),
+                  ),
+                ),
+                Positioned(
+                  top: duSetWidth(13),
+                  child: Image.asset(
+                    "assets/images/logo.png",
+                    fit: BoxFit.none,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: duSetHeight(15)),
+            child: Text(
+              'SECTOR',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: AppColors.primaryText,
+                fontFamily: "Montserrat",
+                fontWeight: FontWeight.w600,
+                fontSize: duSetFontSize(24),
+                height: 1,
+              ),
+            ),
+          ),
+          Text(
+            "news",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: AppColors.primaryText,
+              fontFamily: "Avenir",
+              fontWeight: FontWeight.w400,
+              fontSize: duSetFontSize(16),
+              height: 1,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // 登录表单
+  Widget _buildInputForm() {
+    return Container(
+      width: duSetWidth(295),
+      margin: EdgeInsets.only(top: duSetHeight(49)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          inputTextEdit(
+            controller: _emailController,
+            hintText: 'Email',
+            marginTop: 0,
+            keyboardType: TextInputType.emailAddress,
+          ),
+          inputTextEdit(
+            controller: _passController,
+            hintText: 'Password',
+            marginTop: 15,
+            keyboardType: TextInputType.visiblePassword,
+            isPassword: true,
+          ),
+          Container(
+            height: 44,
+            margin: EdgeInsets.only(top: duSetHeight(15)),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  child: btnFlatButtonWidget(
+                    onPressed: () {},
+                    title: 'Sign up',
+                    gbColor: AppColors.thirdElement,
+                  ),
+                ),
+                SizedBox(width: 15),
+                Expanded(
+                  child: btnFlatButtonWidget(
+                    onPressed: () {},
+                    title: 'Sign in',
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 15),
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              'Fogot password?',
+              style: TextStyle(
+                color: AppColors.secondaryElementText,
+                fontFamily: "Avenir",
+                fontWeight: FontWeight.w400,
+                fontSize: duSetFontSize(16),
+                height: 1,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // 第三方登录
+  Widget _buildThirdPartyLogin() {
+    return Container(
+      width: duSetWidth(295),
+      margin: EdgeInsets.only(bottom: duSetHeight(40)),
+      child: Column(
+        children: [
+          // title
+          Text(
+            "Or sign in with social networks",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: AppColors.primaryText,
+              fontFamily: "Avenir",
+              fontWeight: FontWeight.w400,
+              fontSize: duSetFontSize(16),
+            ),
+          ),
+          SizedBox(height: 20),
+          Container(
+            height: 44,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  child: btnImageButtonWidget(
+                    onPressed: () {},
+                    iconFileName: 'twitter',
+                  ),
+                ),
+                SizedBox(width: 15),
+                Expanded(
+                  child: btnImageButtonWidget(
+                    onPressed: () {},
+                    iconFileName: 'google',
+                  ),
+                ),
+                SizedBox(width: 15),
+                Expanded(
+                  child: btnImageButtonWidget(
+                    onPressed: () {},
+                    iconFileName: 'facebook',
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  // 注册按钮
+  Widget _buildSignupButton() {
+    return Container(
+      width: duSetWidth(295),
+      height: 44,
+      margin: EdgeInsets.only(bottom: duSetHeight(20)),
+      child: btnFlatButtonWidget(
+        onPressed: () {},
+        gbColor: AppColors.secondaryElement,
+        fontColor: AppColors.primaryText,
+        title: "Sign up",
+        fontWeight: FontWeight.w500,
+        fontSize: 16,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Center(
+        child: Column(
+          children: [
+            _buildLogo(),
+            _buildInputForm(),
+            Spacer(),
+            _buildThirdPartyLogin(),
+            _buildSignupButton(),
+          ],
+        ),
+      ),
+    );
+  }
+}
